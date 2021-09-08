fd9089033151d6b473d5bc3bda979aaa5751717d
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2021-09-08 22:07:32 +0200

    Release-mode (only writeable FS when pressing key on boot)

--- pico/boot.py
+++ pico/boot.py
@@ -1,6 +1,5 @@
 """
-boot.py file for Pico data logging example. If pin GP0 is connected to GND when
-the pico starts up, make the filesystem writeable by CircuitPython.
+If a key is connected to GND when the pico starts up, make the filesystem writeable by host (for configuring/reprogramming).
 """
 import board
 import digitalio
@@ -11,13 +10,11 @@ import sys
 print(f"Checking that no buttons are pressed at boot.")
 
 # If write pin is connected to ground on start-up, CircuitPython can write to CIRCUITPY filesystem.
-if not CONFIG_BUTTON.value:
+if not all(btn.value for btn in btns):
+    print("Not all buttons were 'up'. Make sure to exit in the main script... Exiting in the boot.py-script does nothing in particular...")
+    sys.exit(-1)
+else:
     print("Mounting as read-only for host! Disabling storage and serial port.")
     storage.remount("/", readonly=False)
     storage.disable_usb_drive()
     usb_cdc.disable()
-elif not all(btn.value for btn in btns):
-    print("Not all buttons were 'up'. Exiting")
-    sys.exit(-1)
-else:
-    print("Not pressed. Starting in RW mode.")
